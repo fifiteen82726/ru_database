2bffe62be6ef38b4ab4af3249aa614e88413e2e5
fifiteen82726 <fifiteen82726@gmail.com>
2015-06-29 02:35:16 +0800

    git init

---
+++ Default.aspx
@@ -0,0 +1,91 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<script type="text/javascript" src = "js/bootstrap.min.js"></script>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Final Database Project</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+<div class="container mb">
+	<div class="row">
+		<div class="col-md-12">
+			 <nav class="navbar navbar-static-top navbar-inverse mb">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="http://localhost">Super Video</a>
+            </div>
+            <div id="navbar" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="/">Home</a></li>
+                   
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+			<br><br>
+			<div class="jumbotron">
+				<h1>
+					資料庫期末專題-Super Video
+				</h1>
+				<h3>
+				 	可以選取歌手的名子並選取其歌曲影片播放
+				</h3>
+				<p>
+				<div class="col-md-3">
+				<select class="btn btn-default mbutton mbutton-lg form-control" id="pl-select">
+                <option>Lady Gaga</option>
+                <option>Bruno Mars</option>
+                <option>Maroon5</option>
+              
+           		</select>
+           		</div>
+				<asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+                </asp:DropDownList>
+                <asp:DropDownList ID="DropDownList2" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList2_SelectedIndexChanged" Visible="False">
+                </asp:DropDownList>
+				</p>
+			</div>
+		</div>
+		<br>
+			<asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:s1021704ConnectionString %>" SelectCommand="SELECT [singer_name], [singer_id], [gender] FROM [singer]"></asp:SqlDataSource>
+			<div class="col-md-8">
+				<iframe width="630 " height="470" src="http://www.youtube.com/embed/qrO4YZeyl0I?autoplay=0">
+    			</iframe>
+			</div>
+			<div class="col-md-4">
+				<h2>Lady Gaga</h2> <br>
+				<h3>Song name</h3>
+			</div>
+
+	</div>
+</div>
+
+	
+    </form>
+
+	
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+using System.Data;                  // Dataset
+using System.Data.SqlClient;        // SqlDataAdapter
+ 
+public partial class _Default : System.Web.UI.Page
+{
+    public string youtubeLink = "";
+    static int value = 0;
+    string conn = "Server=140.138.154.29; Database=s1021704; User ID=sy; Password=1234; Trusted_Connection=False;";
+    //宣告 資料庫命令 字串
+    int dropSingerValue=0;
+    int dropVideoValue = 0;
+    int videoId = 0;
+   
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //Response.Write("s" + value);
+        if (DropDownList1.SelectedValue != "")
+        {
+            dropSingerValue = Convert.ToInt32(DropDownList1.SelectedValue);
+           // Response.Write(Convert.ToInt32(DropDownList1.SelectedValue).ToString());
+           
+        }
+       // string strQuery = "SELECT course_id, course_name FROM tbl_course";
+        if (!Page.IsPostBack)
+        {
+            string strQuery = "SELECT singer_id,singer_name,gender FROM singer;";
+            SqlDataAdapter da = new SqlDataAdapter(strQuery, conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+
+                ds.Dispose();
+                DropDownList1.Items.Clear();
+                DropDownList1.DataSource = ds;
+                DropDownList1.DataTextField = "singer_name";
+                DropDownList1.DataValueField = "singer_id";
+                DropDownList1.DataBind();
+                DropDownList1.Items.Insert(0, new ListItem("-- Select Singer --", "0"));
+            }
+            else
+            {
+                Response.Write("no data");
+
+                Response.End();
+            }
+
+        }
+
+        if (DropDownList2.SelectedValue != "")
+        {
+            dropVideoValue = Convert.ToInt32(DropDownList2.SelectedValue);
+            // Response.Write(Convert.ToInt32(DropDownList1.SelectedValue).ToString());
+
+        }
+
+        if (dropVideoValue != 0)
+        {
+            dropVideoValue = Convert.ToInt32(DropDownList2.SelectedValue);
+            Response.Write("hello"+dropVideoValue.ToString());
+            string LinkQuery = "SELECT video_link FROM video where video_id =" + dropVideoValue.ToString() + ";";
+
+            SqlDataAdapter da3 = new SqlDataAdapter(LinkQuery, conn);
+            DataSet ds3 = new DataSet();
+            da3.Fill(ds3);
+            if (ds3.Tables[0].Rows.Count > 0)
+            {
+               Response.Write( ds3.Tables[0].Rows[0][0]);
+               //Response.End();
+               
+            }
+            else
+            {
+                Response.Write("no data");
+                Response.End();
+
+            }
+
+
+        }
+
+        if (DropDownList2.SelectedValue != "")
+        {
+            dropVideoValue = Convert.ToInt32(DropDownList2.SelectedValue);
+            // Response.Write(Convert.ToInt32(DropDownList1.SelectedValue).ToString());
+
+        }
+
+        if (dropSingerValue!= 0 && dropVideoValue ==0)
+        {
+
+            DropDownList2.Visible = true;
+            string videoQuery = "SELECT video_id,video_name FROM video where singer_id =" + dropSingerValue.ToString() + ";";
+
+            SqlDataAdapter da2 = new SqlDataAdapter(videoQuery, conn);
+            DataSet ds2 = new DataSet();
+            da2.Fill(ds2);
+            if (ds2.Tables[0].Rows.Count > 0)
+            {
+                ds2.Dispose(); DropDownList2.Items.Clear();
+                DropDownList2.DataSource = ds2;
+                DropDownList2.DataTextField = "video_name";
+                DropDownList2.DataValueField = "video_id";
+                DropDownList2.DataBind();
+                DropDownList2.Items.Insert(0, new ListItem("-- Select Video --", "0"));
+            }
+            else
+            {
+                Response.Write("no data"); 
+               // Response.End();
+
+            }
+        }
+
+       
+
+    }
+
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+       
+        
+      //  dropSingerValue =  Convert.ToInt32(DropDownList1.SelectedValue);
+     
+
+    }
+    protected void DropDownList2_SelectedIndexChanged(object sender, EventArgs e)
+    {
+       value = Convert.ToInt32(DropDownList2.SelectedValue);
+       Response.Write("S2" + DropDownList2.SelectedValue.ToString());
+     //  Response.End();
+       
+    }
+}
